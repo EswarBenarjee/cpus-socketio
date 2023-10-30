5944570f89d6bdd00cf3d53908361581a75bf8fc
EswarBenarjee <eswar.bane2002.india@gmail.com>
2023-10-30 22:58:19 +0530

    Adding REDIS host

--- server/index.js
+++ server/index.js
@@ -18,8 +18,6 @@ const socketio = require("socket.io");
 const socketMain = require("./socketMain");
 // const expressMain = require('./expressMain');
 
-console.log("Hello Sockets");
-
 const port = 8181;
 const num_processes = require("os").cpus().length;
 // Brew breaks for me more than it solves a problem, so I
@@ -100,9 +98,7 @@ if (cluster.isMaster) {
   // server is assumed to be on localhost:6379. You don't have to
   // specify them explicitly unless you want to change them.
   // redis-cli monitor
-  io.adapter(
-    io_redis({ host: "https://cpus-socketio-api.onrender.com", port: 6379 })
-  );
+  io.adapter(io_redis({ host: "localhost", port: 6379 }));
 
   // Here you might use Socket.IO middleware for authorization etc.
   // on connection, send the socket over to our module with socket stuff
