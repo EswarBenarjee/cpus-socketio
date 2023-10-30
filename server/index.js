bce9beef4a1e3836a997f9004cb252106c17c298
EswarBenarjee <eswar.bane2002.india@gmail.com>
2023-10-30 22:45:05 +0530

    Updating reids host

--- server/index.js
+++ server/index.js
@@ -100,7 +100,9 @@ if (cluster.isMaster) {
   // server is assumed to be on localhost:6379. You don't have to
   // specify them explicitly unless you want to change them.
   // redis-cli monitor
-  io.adapter(io_redis({ host: "http://localhost", port: 6379 }));
+  io.adapter(
+    io_redis({ host: "https://cpus-socketio-api.onrender.com", port: 6379 })
+  );
 
   // Here you might use Socket.IO middleware for authorization etc.
   // on connection, send the socket over to our module with socket stuff
