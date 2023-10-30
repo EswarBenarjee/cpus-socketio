55e7914e7e0707373354ed3767569fdf6c54990b
EswarBenarjee <eswar.bane2002.india@gmail.com>
2023-10-30 20:29:57 +0530

    Updating redis host

--- server/index.js
+++ server/index.js
@@ -98,9 +98,7 @@ if (cluster.isMaster) {
   // server is assumed to be on localhost:6379. You don't have to
   // specify them explicitly unless you want to change them.
   // redis-cli monitor
-  io.adapter(
-    io_redis({ host: "https://cpus-socketio-api.vercel.app/", port: 6379 })
-  );
+  io.adapter(io_redis({ host: "http://localhost", port: 6379 }));
 
   // Here you might use Socket.IO middleware for authorization etc.
   // on connection, send the socket over to our module with socket stuff
