6375e9abaffac2918e8e5e6070f906dcc9684367
EswarBenarjee <eswar.bane2002.india@gmail.com>
2023-10-30 16:26:24 +0530

    MONGOURI

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,8 @@
 const mongoose = require("mongoose");
 
-const config = require("config");
-const db = config.get(
-  "mongodb+srv://eswar:gdYN6TFvXVenMvvm@cluster0.tr2cllj.mongodb.net/?retryWrites=true&w=majority"
-);
-
 mongoose
-  .connect(db)
+  .connect(
+    "mongodb+srv://eswar:gdYN6TFvXVenMvvm@cluster0.tr2cllj.mongodb.net/?retryWrites=true&w=majority"
+  )
   .then((res) => console.log("MongoDB Connected"))
   .catch((err) => console.log("err:", err));
